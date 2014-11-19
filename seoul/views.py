f5b9839827f0ebf5bb9beb33312fda35ac9484cf
she110ff <youngsoo.j@gmail.com>
2014-11-19 16:18:08 +0900

    resend email

--- seoul/views.py
+++ seoul/views.py
@@ -6,6 +6,20 @@ from mezzanine.conf import settings
 from .models import UserCategory
 from cartridge.shop.models import Category
 
+from django.shortcuts import redirect
+from django.template import RequestContext
+from django.template.loader import get_template
+from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import (HttpResponse, HttpResponseServerError,
+                         HttpResponseNotFound)
+
+from mezzanine.conf import settings
+from mezzanine.core.forms import get_edit_form
+from mezzanine.core.models import Displayable, SitePermission
+from mezzanine.utils.cache import add_cache_bypass
+from mezzanine.utils.views import is_editable, paginate, render, set_cookie
+
 def user_subscription(request):
     res = {"result": "fail", "value":"null"}
     if request.method == 'POST':
@@ -31,3 +45,27 @@ def pull_git(request):
     output = sp.Popen(["git", "pull", "origin", "master", "-f"], stdout=sp.PIPE).communicate()[0]
     res = {"result": output, "value":"null"}
     return HttpResponse(json.dumps(res), content_type="application/json")
+
+
+@requires_csrf_token
+def page_not_found(request, template_name="errors/404.html"):
+    """
+    Mimics Django's 404 handler but with a different template path.
+    """
+    context = RequestContext(request, {
+        "STATIC_URL": settings.STATIC_URL,
+        "request_path": request.path,
+    })
+    t = get_template(template_name)
+    return HttpResponseNotFound(t.render(context))
+
+
+@requires_csrf_token
+def server_error(request, template_name="errors/500.html"):
+    """
+    Mimics Django's error handler but adds ``STATIC_URL`` to the
+    context.
+    """
+    context = RequestContext(request, {"STATIC_URL": settings.STATIC_URL})
+    t = get_template(template_name)
+    return HttpResponseServerError(t.render(context))
