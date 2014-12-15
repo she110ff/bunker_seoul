9b45a1340e0de291f1f5abfcac4dbcc6ac99fd5e
she110ff <youngsoo.j@gmail.com>
2014-12-15 09:53:18 +0900

    add gnb

--- seoul/urls.py
+++ seoul/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^timeline/$', login_required(TemplateView.as_view(template_name="seoul/timeline.html"))),
     url(r'^privacy/$', TemplateView.as_view(template_name="seoul/privacy.html")),
     url(r'^subscription/list/$', login_required(TemplateView.as_view(template_name="seoul/subscription.html"))),
+    url(r'^recommend/$', login_required(TemplateView.as_view(template_name="seoul/recommend.html"))),
     url(r'^user/subscription/$', login_required(views.user_subscription)),
     url(r'^product/like/$', login_required(views.user_like_product)),
     url(r'^product/subscription/$', login_required(views.sub_products)),
--- urls.py
+++ urls.py
@@ -42,8 +42,8 @@ urlpatterns += patterns('',
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
-    #url("^$", login_required(direct_to_template), {"template": "seoul/timeline.html"}, name="home"),
-    url("^$", login_required(direct_to_template), {"template": "index.html"}, name="home"),
+    url("^$", login_required(direct_to_template), {"template": "seoul/timeline.html"}, name="home"),
+    #url("^$", login_required(direct_to_template), {"template": "index.html"}, name="home"),
 
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
     # ---------------------------------------------
